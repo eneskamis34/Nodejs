300beca574575be67893b0dbfb5cbe0cbedeb06b
= <eneskamis@yandex.com>
2021-01-30 21:32:55 +0300

    cont

--- RoutesOrnek/controllers/admin.js
+++ RoutesOrnek/controllers/admin.js
@@ -6,7 +6,8 @@ exports.getProducts = (req, res, next) => {
     res.render('admin/products', {
         title: 'Admin Products',
         products: products,
-        path: '/admin/products'
+        path: '/admin/products',
+        action: req.query.action
     });
 }
 
@@ -49,5 +50,10 @@ exports.postEditProduct = (req, res, next) => {
     product.description = req.body.description;
 
     Product.Update(product);
-    res.redirect('/admin/products');
+    res.redirect('/admin/products?action=edit');
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+    Product.DeleteById(req.body.productid);
+    res.redirect('/admin/products?action=delete');
 }
