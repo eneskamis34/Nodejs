adb0dcaf0709ac83de6d88901c17cb8852b7918a
= <eneskamis@yandex.com>
2021-02-07 23:13:32 +0300

    cont

--- RoutesOrnek/models/product.js
+++ RoutesOrnek/models/product.js
@@ -1,18 +1,19 @@
 const products = [
     { id: "13213", name: 'Samsung S6', price: '2000', imageUrl: '1.jpg', description: 'iyi telefon', categoryid: "1" },
-    { id: "13214", name: 'Samsung S7', price: '3000', imageUrl: '2.jpg', description: 'iyi telefon', categoryid: "1" },
+    { id: "13214", name: 'Samsung S7', price: '3000', imageUrl: '2.jpg', description: 'iyi telefon', categoryid: "2" },
     { id: "13215", name: 'Samsung S8', price: '4000', imageUrl: '3.jpg', description: 'iyi telefon', categoryid: "1" },
     { id: "13216", name: 'dizüstü bilgisayar', price: '4000', imageUrl: '3.jpg', description: 'iyi bilgisayar', categoryid: "2" },
     { id: "13217", name: 'Buzdolabı', price: '4000', imageUrl: '3.jpg', description: 'iyi buzdolabı', categoryid: "3" }];
 
 module.exports = class Product {
 
-    constructor(name, price, imageUrl, description) {
+    constructor(name, price, imageUrl, description, categoryid) {
         this.id = (Math.floor(Math.random() * 99999) + 1).toString();
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
         this.description = description;
+        this.categoryid = categoryid;
     }
 
     saveProduct() {
@@ -38,6 +39,7 @@ module.exports = class Product {
         products[index].price = product.price;
         products[index].imageUrl = product.imageUrl;
         products[index].description = product.description;
+        products[index].categoryid = product.categoryid;
     }
 
     static DeleteById(id) {
